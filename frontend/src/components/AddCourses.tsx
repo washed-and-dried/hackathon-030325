a6fab90d86f6c10f1e973cbff426e70909a667df
arshpsps <arshh.gill@outlook.com>
2025-03-03 15:04:06 +0530

    courses, addCourses

--- frontend/src/components/AddCourses.tsx
+++ frontend/src/components/AddCourses.tsx
@@ -1,7 +1,31 @@
-export default function AddCourses(){
+async function subCourse(uid, courseID) {
+  const url = `http://localhost:4000/subscribe/${courseID}/${uid}`;
+  const res = await fetch(url, {
+    method: "POST",
+  });
+  if (!res.ok) {
+    console.log("FAILED");
+  }
+}
+
+async function listCourses() {
+  const url = "http://localhost:4000/course/all";
+  const res = await fetch(url, {
+    method: "GET",
+  });
+
+  const data = await res.json();
+
+  return data.map((d) => {
+    const navigate = useN;
     return (
-    <>
-        this is a reallllly long list of courses
-        </>
-    )
+      <>
+        <button onClick={subCourse(uid, d.id)}>{d.name}</button>
+      </>
+    );
+  });
+}
+
+export default function AddCourses() {
+  return <></>;
 }
--- frontend/src/components/Courses.tsx
+++ frontend/src/components/Courses.tsx
@@ -1,15 +1,24 @@
 import CourseItem from "./CourseItem";
 import "./Courses.css";
-import ResourceItem from "./ResourceItem";
+
+async function listCourses(uid) {
+  const url = `http://localhost:4000/user/courses/${uid}`;
+  const res = await fetch(url, {
+    method: "GET",
+  });
+
+  const data = await res.json();
+
+  return data.map((d) => {
+    return <CourseItem courseInfo={d} />;
+  });
+}
 
 export default function Courses() {
   return (
     <>
       <div className="main">
-        <div className="middle-layout">
-          <CourseItem />
-          <ResourceItem /> {/*FIXME: pass state, {title, course, username}*/}
-        </div>
+        <div className="middle-layout">{listCourses(uid)}</div>
       </div>
     </>
   );
