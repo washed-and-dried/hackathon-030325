adcc30199283b70b57210277b6d6b9485036ac56
arshpsps <arshh.gill@outlook.com>
2025-03-03 14:50:32 +0530

    resourceItem redirect

--- frontend/src/components/ResourceItem.tsx
+++ frontend/src/components/ResourceItem.tsx
@@ -1,14 +1,22 @@
 import "./ResourceItem.css";
 
-export default function ResourceItem({resource}) {
-  return <>
-    <div className="resourceItem">
-      <div className="resourceItemTitleBox">
-        <span className="resourceItemSubtitle">{resource.course.name}</span>
-        <span className="resourceItemTitle">{resource.name}</span>
+import { useNavigate } from "react-router-dom";
+
+function redirectToPage(resource, navigate) {
+  navigate("/ResourcePage", { state: resource });
+}
+
+export default function ResourceItem({ resource }) {
+  const navigate = useNavigate();
+  return (
+    <>
+      <div className="resourceItem" onClick={(redirectToPage, navigate)}>
+        <div className="resourceItemTitleBox">
+          <span className="resourceItemSubtitle">{resource.course.name}</span>
+          <span className="resourceItemTitle">{resource.name}</span>
+        </div>
+        <span className="resourceItemDescription"> {resource.myUser.name}</span>
       </div>
-      <span className="resourceItemDescription"> {resource.myUser.name}
-      </span>
-    </div>
-    </>;
+    </>
+  );
 }
