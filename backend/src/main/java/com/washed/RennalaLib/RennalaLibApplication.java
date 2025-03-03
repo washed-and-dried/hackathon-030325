cdc6f8c7899fecc34108cad4924a7a7935a8a95d
Bharat Maheshwari <119489267+bharatm29@users.noreply.github.com>
2025-03-03 09:40:12 +0530

    init repo

---
+++ backend/src/main/java/com/washed/RennalaLib/RennalaLibApplication.java
@@ -0,0 +1,13 @@
+package com.washed.RennalaLib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RennalaLibApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RennalaLibApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/washed/RennalaLib/RennalaLibApplicationTests.java
@@ -0,0 +1,13 @@
+package com.washed.RennalaLib;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RennalaLibApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
